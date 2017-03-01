38f0be17a9080752b019d3f8dc34e379510ea9e5
yusufyogamadya <yusufyoga17@gmail.com>
2017-03-01 20:03:27 +0700

    Add files via upload

---
+++ Pertemuan1/HelloWorld.bat
@@ -0,0 +1,6 @@
+/*program hello world*/
+class HelloWorld {
+	public class void main(String argv[]){
+	System.out.println("Hello World");
+	}
+}
---
+++ Pertemuan2/mahasiswa.bat
@@ -0,0 +1,8 @@
+/*program mahasiswa*/
+class mahasiswa {
+	private static void main(String argv[]){
+		System.out.println("Nama Lengkap : Yusuf Yoga Madya Sambodo");
+		System.out.println("NIM	: 10115333");
+		System.out.println("Alamat : Jln.Hergamanah Secapa-AD ");
+	}
+}
