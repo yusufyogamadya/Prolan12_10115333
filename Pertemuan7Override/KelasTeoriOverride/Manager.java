8adea11034e631ee232a0371fe7656441f485b66
yusufyogamadya <yusufyoga17@gmail.com>
2017-04-04 22:27:43 +0700

    Add files via upload

---
+++ Pertemuan7Override/KelasTeoriOverride/Manager.java
@@ -0,0 +1,13 @@
+class Manager {
+    public String nama,jabatan,tugas;
+    int gaji;
+    public void Manager1(){
+        System.out.println("\t\t-----------------------------------------------");
+        System.out.println("\t\t                    Data Manager               ");
+        System.out.println("\t\t-----------------------------------------------");
+    }
+
+	public void alamat(){
+		System.out.println("Jln.Hergamanah 152,Kota Bandung");
+	}
+}
